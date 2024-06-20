3c765ca430479b3486468656ebfef16bcc942d2c
xg <maple.hl@gmail.com>
2024-06-20 15:31:50 +0800

    commands for alacritty and wezterm

--- justfile
+++ justfile
@@ -1,16 +1,29 @@
 
-watch-code:
-  watchexec -e py -r -c -- just kill
+watch-and-kill:
+  watchexec -w . -e py -- just kill
 
 watch-runner-process:
-  procs xg -w -W 1
+  procs -c disable xg\/runner\.py -w -W 1
 
 get-pid:
-  ps -ef | rg -i -e '.*python.*runner\.py' | awk -F' ' '{print $2}'
+  @ps -ef | rg -i -e '.*python.*xg\/runner\.py'
+  @ps -ef | rg -i -e '.*python.*xg\/runner\.py' | awk -F' ' '{print $2}'
 
+get-running:
+  @ps -ef | rg -i -e '.*xg\/runner\.py'
+
+nothing:
   # procs -c disable --no-header runner.py | awk -F' ' '{print $1}'
   # procs -c disable --no-header runner.py
  
 kill:
-  kill -9 $(ps -ef | rg -i -e '.*python.*runner\.py' | awk -F' ' '{print $2}')
+  #!/usr/bin/env bash
+
+  PID=$(ps -ef | rg -i -e '.*python.*xg\/runner\.py' | awk -F' ' '{print $2}')
+  if [[ -z $PID ]]; then
+    echo "process not found"
+  else
+    kill $PID
+    echo "process $PID killed"
+  fi
 
--- runner.py
+++ runner.py
@@ -10,7 +10,6 @@ krunner5 package
 methods: /usr/share/dbus-1/interfaces/kf5_org.kde.krunner1.xml
 """
 
-
 from datetime import datetime
 import dbus
 import dbus.service
@@ -21,7 +20,11 @@ from gi.repository import GLib
 
 import logging
 
-logging.basicConfig(level=logging.INFO, filename='/tmp/x-runner.log', format='%(asctime)s - %(filename)s - %(lineno)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO,
+    filename="/tmp/x-runner.log",
+    format="%(asctime)s - %(filename)s - %(lineno)s - %(message)s",
+)
 log = logging.getLogger(__name__)
 
 
@@ -44,14 +47,28 @@ class Runner(dbus.service.Object):
 
     def __init__(self) -> None:
         """Create dbus service"""
-        dbus.service.Object.__init__(self, dbus.service.BusName(SERVICE, dbus.SessionBus()), OBJPATH)
+        dbus.service.Object.__init__(
+            self, dbus.service.BusName(SERVICE, dbus.SessionBus()), OBJPATH
+        )
 
-        log.info('__init__, initializing ...')
+        log.info("__init__, initializing ...")
+        self.hosts = [
+            "a34",
+            "n33",
+            "n35",
+            "bpi",
+            "rpi",
+            "joe",
+            "acer",
+            "ali",
+            "ss-dev",
+            "mi2",
+        ]
 
-    @dbus.service.method(IFACE, in_signature='s', out_signature='a(sssida{sv})')
+    @dbus.service.method(IFACE, in_signature="s", out_signature="a(sssida{sv})")
     def Match(self, query: str):
         """This method is used to get the matches and it returns a list of tuples"""
-        log.info("Received a Match: [%s]", query)
+        log.info("==== Received a Match: [%s]", query)
 
         # [(ID, Text, IconName, Type (Plasma::QueryType), Relevance(0-1), Properties (subtext, category and urls))]
         # - ID
@@ -72,23 +89,59 @@ class Runner(dbus.service.Object):
         #      has alpha, bits per sample, number of channels, pixmap data.
         #    - multiline (boolean). If the text should be displayed as styled multiline text.
 
-        log.info('matches by (ID, Text, IconName, Type, Relevance, Properties)')
+        log.info("matches by (ID, Text, IconName, Type, Relevance, Properties)")
 
         results = []
-        if query.startswith('wez') or 'theme' in query:
-            log.info('wez theme updating request')
-            results = results + [
-                ('wez-theme', 'change theme', "user-busy-symbolic", 100, 1, {'subtext': 'Update Wez Theme', 'actions': ['start']})
+        if query.startswith("wez") or "color" in query:
+            log.info("wez theme updating request")
+            results += [
+                (
+                    "wez-theme",
+                    "Wezterm Colorscheme",
+                    "user-busy-symbolic",
+                    100,
+                    1,
+                    {"subtext": "Update Wez Theme", "actions": ["start"]},
+                )
             ]
 
-        results = results + [
-            # (hostname, f'{hostname} - {datetime.now().strftime("%H:%M:%S")}', "user-busy-symbolic", 100, rel/10, {'subtext': 'SubText'})
-            # for rel, hostname in enumerate(['a34', 'a46'])
-        ]
-        log.info('results: %s', results)
+        for hostname in self.hosts:
+            if hostname in query:
+                results += [
+                    (
+                        f"wezterm:{hostname}",
+                        f"wezterm connect {hostname}",
+                        "user-busy-symbolic",
+                        100,
+                        0,
+                        {"subtext": ""},
+                    ),
+                    (
+                        f"alacritty:{hostname}",
+                        f"alacritty ssh {hostname}",
+                        "user-busy-symbolic",
+                        100,
+                        1,
+                        {"subtext": f"alacritty ssh {hostname}"},
+                    ),
+                ]
+
+        if query.startswith("wez"):
+            results = results + [
+                (
+                    hostname,
+                    f"wez to {hostname}",
+                    "user-busy-symbolic",
+                    100,
+                    rel / 10,
+                    {"subtext": "SubText"},
+                )
+                for rel, hostname in enumerate(self.hosts)
+            ]
+        log.info("match results: %s", results)
         return results
 
-    @dbus.service.method(IFACE, out_signature='a(sss)')
+    @dbus.service.method(IFACE, out_signature="a(sss)")
     def Actions(self):
         """
         Returns a list of actions supported by this runner.
@@ -99,26 +152,40 @@ class Runner(dbus.service.Object):
           - Text
           - IconName
         """
-        log.info(f"{'+' * 10} actions, Received an Actions query, called upon initialization")
+        log.info(
+            f"{'+' * 10} actions, Received an Actions query, called upon initialization"
+        )
         # ID, Text, IconName
         return [
             ("start", "Start the VM", "media-playback-start"),
-            ("stop", "Stop the VM", "media-playback-stop")
+            # ("stop", "Stop the VM", "media-playback-stop"),
+            # ("kill", "ss-dev", "user-busy-symbolic"),
         ]
 
-    @dbus.service.method(IFACE, in_signature='ss')
+    @dbus.service.method(IFACE, in_signature="ss")
     def Run(self, match_id: str, action_id: str, *args, **kwargs):
         # match_id is the ID for `Match` record
         # action_id: The action ID to run. For the default action this will be empty, the ID from `Actions` method
-        log.info("run, received a Run command, match_id: %s, action_id: %s, %s, %s", match_id, action_id, args, kwargs)
-        if match_id == 'wez-theme':
-            subprocess.run(['touch', '-h', '/home/maple/.config/wezterm/wezterm.lua'])
-            log.info('touched')
+        log.info(
+            "run, received a Run command, match_id: %s, action_id: %s, %s, %s",
+            match_id,
+            action_id,
+            args,
+            kwargs,
+        )
+        if match_id == "wez-theme":
+            subprocess.run(["touch", "-h", "/home/maple/.config/wezterm/wezterm.lua"])
+            log.info("touched")
+
+        if match_id.startswith("alacritty:"):
+            hostname = match_id.split(":")[1]
+            subprocess.run(["alacritty", "-e", "ssh", hostname])
+            log.info("alacritty ssh to %s", hostname)
 
     @dbus.service.method(IFACE)
     def Teardown(self, *args, **kwargs):
         # this session is done
-        log.info('done, %s, %s', args, kwargs)
+        log.info("done, %s, %s", args, kwargs)
 
 
 runner = Runner()
